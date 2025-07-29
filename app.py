016270e3425281fc952867d7e77853287c739431
siva <siva@example.com>
2025-07-29 21:19:56 +0530

    updated code

--- app.py
+++ app.py
@@ -101,6 +101,13 @@ def root():
 def check_phone(phone_number):
     existing_client = Client.query.filter_by(phone=phone_number).first()
     return jsonify({'exists': existing_client is not None})
+
+@app.route("/addclient")
+def add_client():
+    # Fetch existing phone numbers from DB
+    phones = [client.phone for client in Client.query.all()]
+    return render_template("addclients.html", existing_phones=phones)
+
 @app.route('/home')
 def home():
     if session.get('admin_logged_in'):
